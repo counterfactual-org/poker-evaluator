f31db9650bb10e2cb288e10911c5721b12ee4a9e
LevX <team@levx.io>
2024-09-12 17:08:59 +0900

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 source .env
-forge script DeployCommon     --rpc-url $RPC_URL --broadcast
-forge script DeployEvaluator5 --rpc-url $RPC_URL --broadcast
-forge script DeployEvaluator7 --rpc-url $RPC_URL --broadcast
-forge script DeployEvaluator9 --rpc-url $RPC_URL --broadcast
+forge script DeployCommon     --rpc-url $RPC_URL --broadcast --slow
+forge script DeployEvaluator5 --rpc-url $RPC_URL --broadcast --slow
+forge script DeployEvaluator7 --rpc-url $RPC_URL --broadcast --slow
+forge script DeployEvaluator9 --rpc-url $RPC_URL --broadcast --slow
