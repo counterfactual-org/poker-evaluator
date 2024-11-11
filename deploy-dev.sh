ff1c641694a186911e6511d1271d3f56657820a3
LevX <team@levx.io>
2024-11-12 06:30:29 +0700

    chore: deploy to arbitrum mainnet

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,6 +4,4 @@ source .env
 rm deployments/50161.json
 RPC_URL=http://localhost:50161
 forge script DeployCommon     --rpc-url $RPC_URL --broadcast --slow
-forge script DeployEvaluator5 --rpc-url $RPC_URL --broadcast --slow
-forge script DeployEvaluator7 --rpc-url $RPC_URL --broadcast --slow
 forge script DeployEvaluator9 --rpc-url $RPC_URL --broadcast --slow
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 
 source .env
 forge script DeployCommon     --rpc-url $RPC_URL --broadcast --slow
-forge script DeployEvaluator5 --rpc-url $RPC_URL --broadcast --slow
-forge script DeployEvaluator7 --rpc-url $RPC_URL --broadcast --slow
 forge script DeployEvaluator9 --rpc-url $RPC_URL --broadcast --slow
